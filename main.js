378b6b23631d06a6a241ec81cd68d5f75b991427
nic <93119331+NicohIas@users.noreply.github.com>
2022-03-31 18:07:02 +1000

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,8 @@ function newApp() {
   mainWindow = new BrowserWindow({
     width: 1920,
     height: 1080,
-    icon: __dirname + './addatives/physed-logo.png'
+    icon: __dirname + './addatives/physed-logo.png',
+    fullscreen: true
   });
   mainWindow.loadURL(`file://${__dirname}/index.html`);
 }
