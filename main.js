f64300e22a0da67066cf2966bce98d54d3532640
nic <93119331+NicohIas@users.noreply.github.com>
2022-04-03 19:15:15 +1000

    Add files via upload

--- main.js
+++ main.js
@@ -9,6 +9,29 @@ function newApp() {
     fullscreen: true
   });
   mainWindow.loadURL(`file://${__dirname}/index.html`);
+  
+  const clientID = '960074343222747187';
+  const discord = require('discord-rpc');
+  const RPC = new discord.Client({transport: 'ipc'});
+
+  discord.register(clientID);
+
+  (async () => {
+    RPC.on('ready', async () => {
+      await RPC.setActivity({
+        details: 'Sandbox',
+        state: 'Simulating Physics',
+        largeImageKey: 'physed-logo',
+        largeImageText: 'github.com/NicohIas',
+        startTimestamp: Date.now(),
+        instance: false
+      }).catch(err => console.error(err));
+
+      onsole.log('RPC Ready');
+    });
+    await RPC.login({clientId: clientID}).catch(err => console.error(err));
+    await RPC.connect().catch(err => console.error(err));
+  })();
 }
 
 app.on("ready", newApp);
